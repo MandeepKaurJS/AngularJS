bc9f979908cc2728d862df689569e9ebe00d6edc
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-05-27 16:06:06 -0700

    update

---
+++ WebApplicationWithAngular/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationWithAngular.Startup))]
+namespace WebApplicationWithAngular
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
